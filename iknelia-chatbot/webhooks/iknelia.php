b6b343dc19bcf0462506f4876d21b46a1707c269
DavidMerino25 <deividrocan2508@gmail.com>
2022-10-14 16:32:31 -0500

    libreria de chatbot implementada

--- iknelia-chatbot/webhooks/iknelia.php
+++ iknelia-chatbot/webhooks/iknelia.php
@@ -1,4 +1,17 @@
 <?php
-echo("Hola");
+//Incluimos la libreria
+include_once "../libreria_dialogflow.php";
+credenciales('iknelia','25082000');
+
+if (intent_recibido('test')) {
+    $valor1 = obtener_variables()['numero1'];
+    $valor2 = obtener_variables()['numero2'];
+
+    $resulado = $valor1 + $valor2;
+
+    enviar_texto("El resultado es: $resulado");
+    
+}
+
 
 ?>
